b4e4f500d7702e2ac5c09a4344b45df1c50c1abd
katholikos <308997603@qq.com>
2019-04-08 15:05:35 +0800

    f

---
+++ .temp/pages/index/index.js
@@ -0,0 +1,28 @@
+import Taro from '@tarojs/taro-h5';
+import { Component } from "@tarojs/taro-h5";
+import Nerv from "nervjs";
+import { View, Text } from '@tarojs/components';
+import './index.less';
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  };
+
+  componentWillMount() {}
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  render() {
+    return <View className="index">
+        <Text>Hello world!</Text>
+      </View>;
+  }
+}
